fe7b0d83b102f8be9cf1343af19a766fa10af431
aminamarovac <aminamarovac@gmail.com>
2022-08-06 13:46:56 +0200

    it camp

---
+++ JavaScript/JavaScript 2.cas/app.js
@@ -0,0 +1,48 @@
+// JavaScript Output
+//Postoji nekoliko nacina da prikazemo podatke u JavScriptu:
+// 1. innerHTML
+//primer:
+document.getElementById ("paragraf").innerHTML = 
+"paragraf dodat preko JavaScript-a";
+// 2. document.write () -koristi se za testiranje koda.
+// 3. alert ()-ova metoda nam salje upozorenje
+// Primer:
+//alert ("Pozdrav");
+// 4. console.log () - najcesce koriscena funkcija za prikazivanje
+//                     stvari u konzoli.
+// Primer:
+
+console.log("Pozdrav iz konzole");
+// JavaScript Identifiers/Names
+// JavaScript names (imena varijabli) moraju pocinjati sa:
+// 1.slovom (velikim ili malim)
+// 2.dollar sign ($)
+// 3.underscore(donja crta) _
+// Napomena
+// Promenljiva ne moze pocinjati brojem.Brojevi se mogu naci kao
+// drugi,treci...karakter u nazivu promenljive.
+
+//JavaScript is Case Sensitive!!!  (razlikuje mala i velika slova)
+//"a" je razlicito od "A"
+
+//     JavaScript varijable
+
+// Postoji nekoliko nacina da deklerisemo varijablu:
+// Koriscenjem var kljucne reci (keyword)
+var x; // deklaracija
+x=10;  //dodela vrednosti promenljivoj x (inicijalizacija)
+var x=10; //odjednom izvrsena deklaracija i inicijalizacija
+// 2.Koriscenjem let kljucne reci (keyword)
+let x;//dekleracija
+x=10; //dodela vrednosti promenljivoj x (inicijalizacija)
+let x =10; //odjednom izvrsena dekleracija i inicijalizacija
+
+
+//3.Koriscenjem const kljucne reci (keyword)
+const x=10;//kod const kljucne reci odjednom se mora
+//izvrsiti dekleracija i inicijalizacija
+
+
+//Varijable (promenljive)su kontejneri za skladistenje vrednosti.
+
+
