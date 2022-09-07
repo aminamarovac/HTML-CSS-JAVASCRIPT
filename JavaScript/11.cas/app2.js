05ee383bcd90ca2ca2d9150a3c2ad678b11ac58c
aminamarovac <aminamarovac@gmail.com>
2022-09-07 20:59:01 +0200

    funkcije

--- JavaScript/11.cas/app2.js
+++ JavaScript/11.cas/app2.js
@@ -1,5 +1,7 @@
 //FUNKCIJE
 
+//DRY(Don't repeat yourself)
+
 //FUNKCIJE predstavljaju blok koda koji se izvrsava samo prilikom pozivanja.
 
 //Nacini zapisivanja funkcije:
@@ -24,3 +26,82 @@ return prviBroj+drugiBroj;
 }
 //Prilikom pozivanja funkcije u zagradi stavljamo argumente na osnovu kojih se funkcija izvrsava.
 console.log(zbirDvaBroja(5,14));
+
+
+function povrsina(prvaVrednost,drugaVrednost){
+    if(prvaVrednost===drugaVrednost){
+    return "Povrsina kvadrata iznosi:" + prvaVrednost*drugaVrednost;
+    }else{
+        return "Povrsina pravougaonika iznosi:" + prvaVrednost*drugaVrednost;
+    }
+}
+console.log(povrsina(15,2));
+console.log(povrsina(4,4));
+
+
+//2.nacin
+const myfunction = function() {
+    //radnja koja se obavlja
+}
+
+//Napraviti funkciju koja vraca zbir kvadrata dva broja.
+//Druga vrednost je po defaultu 1.
+
+//Prvi nacin:
+
+const mySecondFunction = function (prva,druga) {
+    if (druga===undefined){
+        return prva**2 + 1
+    }else{
+        return prva**2 + druga**2;
+    }
+}
+console.log(mySecondFunction(5));
+console.log(mySecondFunction(5,4));
+
+//Drugi nacin:
+
+const mySecondFunction2 = function(prva,druga=1){
+    return prva**2+druga**2;
+}
+console.log(mySecondFunction2(3));
+console.log(mySecondFunction2(3,3));
+
+//Ako neka funkcija ima defaultnu vrednost,ta vrednost se staVLJA NA POSLEDNJEM MESTU.
+//Ako ih je vise,na poslednjim pozicijama.
+//Ne sme se desiti slucaj da prva vrednost bude defaultna,a nakon nje ona koja nema defaultnu vrednost.
+
+
+//arrow function:
+
+const arrowFunc= () => {
+    //kod za izvrsavanje
+};
+
+
+//Napraviti funkciju koja na osnovu unetog argumenta vraca jedno od 4 stanja:
+//Ako je unet pozitivan broj ispisuje poruku:"ARGUMENT JE POZITIVAN BROJ".
+//Ako je unet negativan broj ispisuje poruku:"ARGUMENT JE NEGATIVAN BROJ".
+//Ako nije unet broj,ispisuje poruku:"ARGUMENT NIJE BROJ".
+
+
+const arrowFunc1= (brojj) => {
+    if(typeof brojj!=="number"){
+        return "ARGUMENT NIJE BROJ."
+    }  else if(brojj>0){
+        return "ARGUMENT JE POZITIVAN BROJ."
+    }else if(brojj<0){
+        return "ARGUMENT JE NEGATIVAN BROJ."
+    }else if(brojj===0){
+        return "ARGUMENT JE NULA."
+    }
+
+}
+console.log(arrowFunc1(5))
+console.log(arrowFunc1(-2))
+console.log(arrowFunc1(0));
+console.log(arrowFunc1("2"));
+
+
+
+
