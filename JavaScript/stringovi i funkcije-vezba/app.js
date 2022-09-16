306404771eb00b58b2dc8349740c78ca11bd92db
aminamarovac <aminamarovac@gmail.com>
2022-09-16 12:47:26 +0200

    vezba

--- JavaScript/stringovi i funkcije-vezba/app.js
+++ JavaScript/stringovi i funkcije-vezba/app.js
@@ -1,4 +1,18 @@
-
+function zadatak () {
+    let nekaRecenica=prompt("Unesite neku recenicu:");
+    let duzina=nekaRecenica.length;
+    let polaDuzine=nekaRecenica/2;
+    let prviDeo=nekaRecenica.substr(0,polaDuzine).toUpperCase();
+    let drugiDeo=nekaRecenica.substr(polaDuzine,duzina).toLowerCase();
+    let prva=nekaRecenica.toUpperCase();
+    let druga=nekaRecenica.toLowerCase();
+    let treca=prviDeo.concat(drugiDeo);
+    let cetvrta=nekaRecenica.replace(/skola/gi,"fakultet");
+    let peta=nekaRecenica.substring(0,10);
+    let sesta=nekaRecenica.slice(-10,duzina);
+    return prva.concat("\n",druga,"\n",treca,"\n",cetvrta,"\n",peta,"\n",sesta);
+}
+console.log(zadatak());
 function prvaFunkcija(){
     return 9;
 }
@@ -135,3 +149,18 @@ let sentence11="Sutra ce ";
 let sentence22="biti kisovito.";
 let sentence11And22=sentence11.concat(sentence22);
 console.log(sentence11And22);
+
+
+//Napraviti funkciju koja trazi od korisnika da unese neku recenicu,zatim funkcija vraca
+//datu recenicu u vise oblika(spojenu u jedan string).
+// 1.recenicu ispisanu velikim slovima
+// 2.recenicu ispisanu malim slovima
+// 3.pola recenice ispisano velikim slovima,pola malim slovima (length se primenjuje)
+// 4.Ako se u recenici nadje rec 'skola'(bilo gde),neka bude zamenjena recju 'fakultet' .
+// 5.neka bude ispisano prvih 10 karaktera unete recenice.
+// 6.neka bude ispisano poslednjih 10 karaktera unete recenicee.
+
+
+
+
+
