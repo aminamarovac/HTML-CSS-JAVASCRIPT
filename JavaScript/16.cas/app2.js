f8f74eb77111d0437651cf35326a16c53f858cf2
aminamarovac <aminamarovac@gmail.com>
2022-09-20 15:12:30 +0200

    stringovi

--- JavaScript/16.cas/app2.js
+++ JavaScript/16.cas/app2.js
@@ -9,8 +9,16 @@ console.log(brojFlasa())
 
 //Domaci:
 const pokloni = function () {
-    const brojk = prompt ("Unesite broj cokoladica u jednom poklonu: ");
-    const brojn = prompt ("Unesite ukupan broj cokoladica: ");
-    return Math.floor(brojn / brojk);
+    const brojk = +prompt ("Unesite broj cokoladica u jednom poklonu: ");
+    const brojn = +prompt ("Unesite ukupan broj cokoladica: ");
+    if(isNaN(brojk)|| isNaN(brojn)){
+    return "Niste uneli broj cokoladica.";
+}else if(brojk<1 || brojk>20){
+    return "Broj cokoladica prema jednom poklonu mora biti izmedju 1 i 20.";
+}else if(brojn<0 || brojn>1000){
+    return "Ukupan broj cokoladica mora biti izmedju 1 i 1000.";
+}else{
+    return Math.floor(brojn/brojk);
+}
 }
 console.log(pokloni());
