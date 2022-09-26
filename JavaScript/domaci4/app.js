1e6308f52243c50d58c97a38884fc0fd4900a74e
aminamarovac <aminamarovac@gmail.com>
2022-09-26 20:48:23 +0200

    domaci

--- JavaScript/domaci4/app.js
+++ JavaScript/domaci4/app.js
@@ -17,23 +17,33 @@
 //console.log(karakteri());
 
 //2.zadatak
-const firstByFirst=()=>{
+const firstByFirst=()=> {
     const A=prompt("Unesite recenicu A:");
+    const duzina1=A.length;
     const B=prompt("Unesite recenicu B:");
+    const duzina2=B.length;
     let C="";
     //Sintaksa za ternary operator
     //USLOV ? vrednost u slucaju ispunjenja uslova:vrednost u slucaju neispunjenja uslova
-    let duzina=A.length>B.length? A.length : B.length;
+    let duzina=duzina1>duzina2? duzina1 : duzina2;
     for(let i=0;i<duzina;i++){
-        
-
-    }
-    console.log(firstByFirst());
-
-
-
-
-
-
-
+        C+=A[i] + B[i];
+        if(i>=duzina1){
+            C+=B[i];
+        }else if(i>=duzina2){
+            C+=A[i];
+        }
+        else{
+            C+=A[i]+B[i];
+        }
 }
+return C;
+}
+    console.log(firstByFirst());
+    
+
+
+
+
+
+
