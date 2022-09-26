8d66e8e861e1e65d6d6be94e495828d5629a445c
aminamarovac <aminamarovac@gmail.com>
2022-09-26 20:33:29 +0200

    domaci

--- JavaScript/domaci4/app.js
+++ JavaScript/domaci4/app.js
@@ -1,17 +1,39 @@
 //1.zadatak
-function karakteri(){
-let recenica = prompt ("Zameniti karaktere");
-let novaRecenica = "";
-let duzina=recenica.length;
-for (let i = 0; i < duzina; i++) {
-if (duzina % 2 === 1 && i === duzina- 1) {
-    novaRecenica += recenica[duzina - 1];
-} else if (i % 2 === 0) {
-    novaRecenica += recenica[i + 1];
-} else if (i % 2 === 1) {
-    novaRecenica += recenica[i - 1];
-}
+//function karakteri(){
+//let recenica = prompt ("Zameniti karaktere");
+//let novaRecenica = "";
+//let duzina=recenica.length;
+//for (let i = 0; i < duzina; i++) {
+//if (duzina%2===1 && i===duzina-1) {
+   // novaRecenica += recenica[duzina - 1];
+//} else if (i%2===0) {
+  //  novaRecenica += recenica[i + 1];
+//} else if (i % 2 === 1) {
+    //novaRecenica += recenica[i - 1];
+//}
+   // }
+   // return novaRecenica;
+//}
+//console.log(karakteri());
+
+//2.zadatak
+const firstByFirst=()=>{
+    const A=prompt("Unesite recenicu A:");
+    const B=prompt("Unesite recenicu B:");
+    let C="";
+    //Sintaksa za ternary operator
+    //USLOV ? vrednost u slucaju ispunjenja uslova:vrednost u slucaju neispunjenja uslova
+    let duzina=A.length>B.length? A.length : B.length;
+    for(let i=0;i<duzina;i++){
+        
+
     }
-    return novaRecenica;
+    console.log(firstByFirst());
+
+
+
+
+
+
+
 }
-console.log(karakteri());
