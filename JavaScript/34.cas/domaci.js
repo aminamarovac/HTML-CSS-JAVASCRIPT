cc34e4392422d0c144293711735dbd73437e6f45
aminamarovac <aminamarovac@gmail.com>
2022-10-30 19:56:57 +0100

    nizovi

---
+++ JavaScript/34.cas/domaci.js
@@ -0,0 +1,9 @@
+// Domaci zadatak:
+// 1. Zadatak
+// Sabrati sve godine tj. elemente niza a nakon toga podeliti sa brojem elemenata.
+// tj. vratiti prosecnu starost godina.
+
+// 2. Zadatak
+// Write a function that converts an array of values from miles to kilometres using the map method.
+// In the end, add the kilometres up in a new variable called "totalDistanceInKilometers" and
+// return this variable.
