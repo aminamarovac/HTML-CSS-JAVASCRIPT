078d12dd341ba433869bfc2fbb9ed6366c5455c8
aminamarovac <aminamarovac@gmail.com>
2022-11-06 18:03:47 +0100

    nizovi

--- JavaScript/37.cas/app.js
+++ JavaScript/37.cas/app.js
@@ -64,5 +64,8 @@ function myFunction2(arr){
     //w3 resource// 
     //codewars
 
-
-
+//Na osnovu stringa koji unosi korisnik napraviti niz, gde cemo ispitati da
+// li se jagoda nalazi unutar tog niza. Ako se jagoda ne nalazi unutar niza, 
+//funkcija neka vrati taj niz poredjan abecedno, s tim sto ce se na prvom mestu
+// dodati broj 10, a na poslednjem broj 100.
+// Dok ako se jagoda nalazi unutar niza vratiti poziciju gde se ona nalazi.
