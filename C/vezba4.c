c27914869e2734e69a886b911b4d54fdd1b4ac2e
aminamarovac <aminamarovac@gmail.com>
2022-11-17 22:20:57 +0100

    #

--- C/vezba4.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-int main(){
-printf("On ima %d godina,a ona %.1f\n",15,18.5);
-printf("%.4f %.3f %.2f %.1f\n",3.30,3.30,3.30,3.30);
-printf("%s %d %s %s %.1f %c %c","On ima",14,"godina","a ona",62.3,'k','g');
-return 0;
-}
