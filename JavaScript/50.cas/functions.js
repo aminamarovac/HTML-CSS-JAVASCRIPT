8462de1e28562820beeba2085288f69b6eebed95
aminamarovac <aminamarovac@gmail.com>
2022-12-12 22:13:03 +0100

    exportovanje modula,zadaci...

---
+++ JavaScript/50.cas/functions.js
@@ -0,0 +1,16 @@
+
+//export const zbir=function(arg1,arg2){
+    const zbir=function(arg1,arg2){
+        return arg1+arg2
+    };
+
+    const br=5;
+    export default zbir;
+    export{ br };
+    //export default zbir;
+
+// Postoje 2 nacina za eksportovanje modula:
+//export zbir()
+//export default zbir;
+
+//export default mozemo iskoristiti samo jednom u fajlu.
