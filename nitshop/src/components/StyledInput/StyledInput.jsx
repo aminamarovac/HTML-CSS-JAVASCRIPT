b8e03f3f2779bca13a13583e113461da2949e257
aminamarovac <aminamarovac@gmail.com>
2023-02-04 22:37:29 +0100

    #

--- nitshop/src/components/StyledInput/StyledInput.jsx
+++
@@ -1,8 +0,0 @@
-import React, { UseState } from "react";
-import "./StyledInput.css";
-
-const StyledInput=(props)=>{
-    return <input onChange={props.onChangeHandler} value={props.value}/>;
-};
-
-export default StyledInput;
